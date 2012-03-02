b9e7a187632721cdbc8eaf9f05961055415a0c6d
Yarden Katz <yarden.katz@gmail.com>
2012-03-01 22:54:56 -0500

    sketches

--- rnaseqlib/run_pipeline.py
+++ rnaseqlib/run_pipeline.py
@@ -6,6 +6,15 @@ import os
 import sys
 import time
 
+import settings
+
+def run_pipeline(settings_filename, output_dir):
+    """
+    Run pipeline given settings file.
+    """
+    settings.load_settings(settings_filename)
+
+
 def main():
     from optparse import OptionParser
     parser = OptionParser()
@@ -17,11 +26,21 @@ def main():
                       help="Output directory.")
     (options, args) = parser.parse_args()
 
-    output_dir = options.output_dir
-    if output_dir != None:
-        output_dir = pathify(output_dir)
-        if not os.path.isdir(output_dir):
-            os.makedirs(output_dir)
+    if options.settings_filename == None:
+        print "Error: need --settings".
+        sys.exit(1)
+        
+    settings_filename = utils.pathify(options.settings)
+    if options.output_dir == None:
+        print "Error: need --output-dir"
+        sys.exit(1)
+        
+    output_dir = utils.pathify(output_dir)
+    if not os.path.isdir(output_dir):
+        os.makedirs(output_dir)
+
+    if options.run != None:
+        run_pipeline(settings_filename, output_dir)
     
 
 if __name__ == '__main__':
