4d934eeb4f9cb3f0885e3617cd43c029e410fe40
Yarden Katz <yarden.katz@gmail.com>
2012-03-01 22:25:09 -0500

    more sketches

--- rnaseqlib/run_pipeline.py
+++ rnaseqlib/run_pipeline.py
@@ -1,27 +1,16 @@
 ##
-## Quality control metrics for RNA-Seq
+## Main driver for running pipeline
 ##
+
 import os
 import sys
 import time
-import utils
-
-from numpy import *
-from scipy import *
-
-
-def compute_qc_metrics(bam_filename, output_dir, settings):
-    """
-    Compute QC metrics.
-    """
-    pass
-
 
 def main():
     from optparse import OptionParser
     parser = OptionParser()
-    parser.add_option("--compute-qc", dest="compute_qc", nargs=1, default=None,
-                      help="Compute QC metrics for RNA-Seq library. Takes as input a BAM file.")
+    parser.add_option("--run", dest="run", nargs=1, default=None,
+                      help="Run pipeline.")
     parser.add_option("--settings", dest="settings", nargs=1, default=None,
                       help="Settings filename.")
     parser.add_option("--output-dir", dest="output_dir", nargs=1, default=None,
