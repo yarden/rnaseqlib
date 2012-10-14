bbcd89844ae020c90337737b586209ca7c7afa14
Yarden Katz <yarden.katz@gmail.com>
2012-10-14 19:19:48 -0400

    init files

---
+++ rnaseqlib/init/download_utils.py
@@ -0,0 +1,52 @@
+##
+## Download utilities
+##
+
+import os
+import sys
+import time
+
+import shutil
+import urllib2
+import posixpath
+
+import rnaseqlib
+
+
+def wget(url):
+    """
+    wget a URL.
+    """
+    t1 = time.time()
+    wget_cmd = "wget \'%s\'" %(url)
+    os.system(wget_cmd)
+    t2 = time.time()
+    print "  Downloading took %.2f minutes." %((t2 - t1)/60.)
+
+
+def download_url(url, output_dir,
+                 binary=True,
+                 basename=None,
+                 unless_exists=True):
+    """
+    Download a url and put it at the desired location.
+    """
+    print "Downloading: %s" %(url)
+    t1 = time.time()
+    url_in = urllib2.urlopen(url)
+    url_name = posixpath.basename(url)
+    if basename != None:
+        url_name = basename
+    output_filename = os.path.join(output_dir, url_name)
+    if unless_exists and os.path.isfile(output_filename):
+        print "  - File exists, skipping."
+        return output_filename
+    if binary:
+        url_out = open(output_filename, "wb")
+    else:
+        url_out = open(output_filename, "w")
+    shutil.copyfileobj(url_in, url_out)
+    t2 = time.time()
+    print "  Downloading took %.2f minutes." %((t2 - t1)/60.)
+    return output_filename
+
