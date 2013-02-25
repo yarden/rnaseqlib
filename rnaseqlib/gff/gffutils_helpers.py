20799d32428f2c8d2489e19e1d61f75efd12077d
Yarden Katz <yarden.katz@gmail.com>
2013-02-25 14:51:38 -0500

    sanitize gffs

--- rnaseqlib/gff/gffutils_helpers.py
+++ rnaseqlib/gff/gffutils_helpers.py
@@ -82,6 +82,7 @@ def create_db(gff_fname, db_fname=None):
 
 
 def write_rec_to_gff(gff_out, record):
+    # Get our own attribute string to avoid duplicates
     gff_out.write("%s\n" %(str(record)))
 
 
@@ -93,15 +94,16 @@ def sanitize_record(record):
 
 
 def fix_up_down_ids(parts):
-    if not (len(parts) == 3 or len(parts) == 2):
-        return parts
     first_part, last_part = parts[0], parts[-1]
     # If it's a minus strand event, then the upstream exon
     # must have a greater coordinate than the downstream
-    if (first_part.id.endswith(".dn") and last_part.id.endswith(".up")) and \
+    if (first_part.id.endswith(".dn") and \
+        last_part.id.endswith(".up")) and \
        (first_part.stop <= last_part.start):
-        first_part.id = "%s.up" %(first_part.id[:-3])
-        last_part.id = "%s.dn" %(last_part.id[:-3])
+        first_part.attributes["ID"] = "%s.up" %(first_part.id[:-3])
+        last_part.attributes["ID"] = "%s.dn" %(last_part.id[:-3])
+    parts[0] = first_part
+    parts[-1] = last_part
     return parts
 
 
@@ -119,10 +121,8 @@ def sanitize_gff(db_fname, output_db_fname, output_dir,
     gff_out = open(gff_out_fname, "w")
     gff_db = gffdb.GFFDB(db_fname)
     print "  - Writing sanitized GFF to: %s" %(gff_out_fname)
-    num_genes = 0
+    t1 = time.time()
     for gene in gff_db.get_gene_objects():
-        print "on gene %d" %(num_genes)
-        num_genes += 1
         gene_rec = gene["gene_rec"]
         # Write gene record
         write_rec_to_gff(gff_out, sanitize_record(gene_rec))
@@ -132,8 +132,10 @@ def sanitize_gff(db_fname, output_db_fname, output_dir,
             write_rec_to_gff(gff_out, sanitize_record(mRNA_obj["record"]))
             parts = list([sanitize_record(part) \
                           for part in mRNA_obj["parts"]])
-            parts = fix_up_down_ids(parts)
-            for part in parts:
+            fixed_parts = fix_up_down_ids(parts)
+            for part in fixed_parts:
                 write_rec_to_gff(gff_out, part)
     gff_out.close()
+    t2 = time.time()
+    print "Sanitizing took %.2f seconds" %(t2 - t1)
     return gff_out_fname, output_db_fname
