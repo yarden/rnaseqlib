27b5bbeefae23ca0a712824b351d935ad6326038
Yarden Katz <yarden.katz@gmail.com>
2013-03-07 17:26:00 -0500

    Fixed order of output for GFF during sanitation

--- scripts/gff_extract_event_seqs.py
+++ scripts/gff_extract_event_seqs.py
@@ -3,13 +3,16 @@ import sys
 import re
 import time
 
+import numpy as np
+
 from collections import defaultdict
 
 import rnaseqlib
-import rnaseqlib.utils import utils
+import rnaseqlib.utils as utils
 
 import misopy
 import misopy.gff_utils as gff_utils
+import misopy.Gene as gene_utils
 
 import string
 from string import maketrans
@@ -94,8 +97,62 @@ def get_flanking_introns_coords(gff_db,
     # regions["dn"] = coords_from_relative_regions(skipped_exon,
     #                                              dn_exon,
     #                                              dn_regions)
+
+def get_event_recs_from_gene(gene_obj, gene_tree):
+    """
+    Given a gene object a tree of gene records,
+    return the GFF records corresponding to the
+    following event parts:
+
+      upstream exon, skipped exon, downstream exon
+
+    Return None if gene cannot be parsed
+    """
+    gene_parts = {}
+    gene_id = gene_obj.label
+    num_isoforms = len(gene_obj.isoforms)
+    # Only consider two-isoform events from GFF
+    if num_isoforms != 2:
+        print "Skipping %s since it does not have two isoforms." \
+              %(gene_id)
+        return gene_parts
+    # Take the long mRNA (usually the first mRNA)
+    long_mRNA, short_mRNA = gene_obj.isoforms[0], gene_obj.isoforms[1]
+    if long_mRNA.len < short_mRNA.len:
+        print "WARNING: Needed to swap mRNAs %s and %s" \
+              %(long_mRNA.label, short_mRNA.label)
+        raise Exception
+        # Swap mRNAs if first is shorter than second
+        long_mRNA, short_mRNA = short_mRNA, long_mRNA
+    long_mRNA_id = long_mRNA.label
+    # Get the GFF record for the current mRNA
+    long_mRNA_tree = gene_tree[gene_id]["mRNAs"][long_mRNA_id]
+    # Check that the mRNA has three exons
+    if len(long_mRNA.parts) != 3:
+        print "Error: Need mRNA %s to have exactly three exons." \
+              %(long_mRNA_id)
+        print "  - Skipping.."
+        return gene_parts
+    # Find the upstream, skipped and downstream exons
+    # Up exon
+    up_exon_id = long_mRNA.parts[0].label
+    up_exon_rec = \
+        long_mRNA_tree["exons"][up_exon_id]
+    # Skipped exon
+    se_exon_id = long_mRNA.parts[1].label
+    se_exon_rec = \
+        long_mRNA_tree["exons"][se_exon_id]
+    # Down exon
+    dn_exon_id = long_mRNA.parts[2].label
+    dn_exon_rec = \
+        long_mRNA_tree["exons"][dn_exon_id]
+    gene_parts["up_exon"] = up_exon_rec
+    gene_parts["se_exon"] = se_exon_rec
+    gene_parts["dn_exon"] = dn_exon_rec
+    return gene_parts
     
-def fetch_seq_from_gff(gff_f, genome_dir, output_dir,
+    
+def fetch_seq_from_gff(gff_fname, fasta_fname, output_dir,
                        with_flanking_introns=False,
                        entries_to_include=["gene",
                                            "mRNA",
@@ -103,102 +160,89 @@ def fetch_seq_from_gff(gff_f, genome_dir, output_dir,
     """
     Fetch sequence from GFF file.
 
+    Outputs:
+
+    (1) GFF file containing an annotation of the sequences.
+
+    (2) FASTA file with the actual sequences.
+
     If asked, fetch the flanking intronic sequences.
     """
-    # Mapping from chromosome IDs to their filenames
-    files = {}
-    # Load GFF file
-    #gff_in = gff_utils.Reader(open(gff_f, "r"))
-    gff_in = gff_utils.GFFDatabase(from_filename=gff_f,
+    # Load GFF genes
+    gff_db = gff_utils.GFFDatabase(from_filename=gff_fname,
                                    reverse_recs=True)
-    file_basename = "%s.fa" %(re.sub("\.gff3?", "",
-                                     os.path.basename(gff_f)))
-    output_filename = os.path.join(output_dir,
+    file_basename = re.sub("\.gff3?", "",
+                           os.path.basename(gff_fname))
+    output_basename = os.path.join(output_dir,
                                    file_basename)
-    print "Outputting sequences to: %s" %(output_filename)
-    output_file = open(output_filename, "w")
-    for record in gff_in:
-        # Chromosome
-        chrom = record.seqid
-        # Type
-        source = record.type
-        # Name
-        name = record.type
-        start, end = record.start, record.end
-        strand = record.strand
-        attributes = record.attributes
-        rec_id = attributes["ID"][0]
-        if "Parent" not in attributes:
-            parent_id = ""
-        else:
-            parent_id = attributes["Parent"][0]
-        # If it's not the kind of entry we were looking for,
-        # skip it
-        if source not in entries_to_include:
+    gff_output_fname = "%s.gff" %(output_basename)
+    fasta_output_fname = "%s.fa" %(output_basename)
+    print "Outputting GFF coordinates to: %s" %(gff_output_fname)
+    if os.path.isfile(gff_output_fname):
+        print "  - Overwriting existing file"
+    print "Outputting sequences to: %s" %(fasta_output_fname)
+    if os.path.isfile(fasta_output_fname):
+        print "  - Overwriting existing file"
+    genes = gene_utils.load_genes_from_gff(gff_fname)
+    gff_out = gff_utils.Writer(open(gff_output_fname, "w"))
+    for gene_id in genes:
+        gene_info = genes[gene_id]
+        gene_tree = gene_info["hierarchy"]
+        gene_obj = gene_info["gene_object"]
+        gene_rec = gene_tree[gene_id]["gene"]
+        ##
+        ## GFF records to write for the current gene
+        ##
+        recs_to_write = []
+        # For mRNA entries, extract the flanking introns of the
+        # alternative exon if asked
+        event_recs = get_event_recs_from_gene(gene_obj, gene_tree)
+        if event_recs is None:
             continue
-        # Skip irrelevant chromosomes
-        if ('random' in chrom) or ('j' in chrom) or ('_' in chrom):
-            continue
-        # Open chromosome file if not open already
-        if chrom not in files:
-            chrom_filename = os.path.join(genome_dir, "%s.fa" %(chrom))
-            if not os.path.isfile(chrom_filename):
-                print "WARNING: %s does not exist" %(chrom_filename)
+        if with_flanking_introns:
+            flanking_intron_coords = \
+                get_flanking_introns_coords(gff_db, long_mRNA_rec)
+            if flanking_intron_coords == None:
                 continue
+            # Fetch upstream intron sequence
+            up_intron_start, up_intron_end = flanking_intron_coords["up"]
+            up_intron_seq = fetch_seq(chrom_file,
+                                      up_intron_start,
+                                      up_intron_end,
+                                      chrom,
+                                      strand)
+            if up_intron_seq != None:
+                seq_id = ":".join(map(str, [chrom, start, end, strand]))
+                seq_id += "_up_intron"
+                output_line = ">%s;%s\n%s\n" %(name, seq_id, up_intron_seq)
+                output_file.write(output_line)
             else:
-                print "Opening: %s" %(chrom_filename)
-            files[chrom] = open(chrom_filename, "r")
-        chrom_file = files[chrom]
-        seq = fetch_seq(chrom_file,
-                        start, end,
-                        chrom, strand)
-        if seq != None:
-            seq_id = ":".join(map(str, [chrom, start, end, strand]))
-            output_line = ">%s;%s;ID=%s;Parent=%s\n%s\n" \
-                %(name, seq_id,
-                  rec_id,
-                  parent_id,
-                  seq)
-            output_file.write(output_line)
-            # For mRNA entries, extract the flanking introns of the
-            # alternative exon if asked
-            if with_flanking_introns:
-                flanking_intron_coords = get_flanking_introns_coords(gff_in,
-                                                                     record)
-                if flanking_intron_coords == None:
-                    continue
-                # Fetch upstream intron sequence
-                up_intron_start, up_intron_end = flanking_intron_coords["up"]
-                up_intron_seq = fetch_seq(chrom_file,
-                                          up_intron_start,
-                                          up_intron_end,
-                                          chrom,
-                                          strand)
-                if up_intron_seq != None:
-                    seq_id = ":".join(map(str, [chrom, start, end, strand]))
-                    seq_id += "_up_intron"
-                    output_line = ">%s;%s\n%s\n" %(name, seq_id, up_intron_seq)
-                    output_file.write(output_line)
-                else:
-                    print "WARNING: Could not fetch upstream intron seq %s, %s, %s" \
-                        %(start, end, strand)
-                # Fetch downstream intron sequence
-                dn_intron_start, dn_intron_end = flanking_intron_coords["dn"]
-                dn_intron_seq = fetch_seq(chrom_file,
-                                          dn_intron_start,
-                                          dn_intron_end,
-                                          chrom,
-                                          strand)
-                if dn_intron_seq != None:
-                    seq_id = ":".join(map(str, [chrom, start, end, strand]))
-                    seq_id += "_dn_intron"
-                    output_line = ">%s;%s\n%s\n" %(name, seq_id, dn_intron_seq)
-                    output_file.write(output_line)
-                else:
-                    print "WARNING: Could not fetch downstream intron seq %s, %s, %s" \
-                        %(start, end, strand)
+                print "WARNING: Could not fetch upstream intron seq %s, %s, %s" \
+                    %(start, end, strand)
+            # Fetch downstream intron sequence
+            dn_intron_start, dn_intron_end = flanking_intron_coords["dn"]
+            dn_intron_seq = fetch_seq(chrom_file,
+                                      dn_intron_start,
+                                      dn_intron_end,
+                                      chrom,
+                                      strand)
+            if dn_intron_seq != None:
+                seq_id = ":".join(map(str, [chrom, start, end, strand]))
+                seq_id += "_dn_intron"
+                output_line = ">%s;%s\n%s\n" %(name, seq_id, dn_intron_seq)
+                output_file.write(output_line)
+            else:
+                print "WARNING: Could not fetch downstream intron seq %s, %s, %s" \
+                    %(start, end, strand)
 
-    output_file.close()
+        # Retrieve the flanking intron coordinates, if asked
+        #     seq_id = ":".join(map(str, [chrom, start, end, strand]))
+        #     output_line = ">%s;%s;ID=%s;Parent=%s\n%s\n" \
+        #         %(name, seq_id,
+        #           rec_id,
+        #           parent_id,
+        #           seq)
+        #     output_file.write(output_line)
 
     
 def fetch_seq(chrom_file,
@@ -223,14 +267,24 @@ def fetch_seq(chrom_file,
     return seq
 
 
+def greeting():
+    print "gff_extract_event_seqs:\nExtract sequences from GFF file " \
+          "encoding alternative events.\n"
+    print "Example:\n"
+    print "gff_extract_event_seqs.py --input-gff events.gff3 --fi mm9.fasta " \
+          "--output-dir event_seqs/\n"
+    print "See --help for options.\n"
+
 
 def main():
     from optparse import OptionParser
     parser = OptionParser()
       
-    parser.add_option("--fetch", dest="fetch", default=None, nargs=2,
-                      help="Fetch sequence from GFF file. Takes as input: "
-                      "(1) GFF filename, (2) genome directory (with no newlines).")
+    parser.add_option("--input-gff", dest="input_gff", default=None, nargs=1,
+                      help="Fetch sequence from GFF events file. Takes as input: "
+                      "GFF filename.")
+    parser.add_option("--fi", dest="fasta_fname", default=None, nargs=1,
+                      help="FASTA filename to fetch sequences from.")
     parser.add_option("--with-flanking-introns", dest="with_flanking_introns",
                       default=False, action="store_true",
                       help="Get sequence of flanking introns relative to skipped exon.")
@@ -250,18 +304,24 @@ def main():
                       help="Output directory.")
     (options, args) = parser.parse_args()
 
-    if options.output_dir == None:
+    if options.output_dir is None:
+        greeting()
         print "Error: need --output-dir to be provided."
         sys.exit(1)
-
+        
     output_dir = options.output_dir
     output_dir = utils.pathify(options.output_dir)
     if not os.path.isdir(output_dir):
         os.makedirs(output_dir)
 
-    if options.fetch != None:
-        gff_filename = utils.pathify(options.fetch[0])
-        genome_dir = utils.pathify(options.fetch[1])
+    if options.input_gff is not None:
+        if options.fasta_fname is None:
+            greeting()
+            print "Error: Must provide input fasta file with --fi."
+            sys.exit(1)
+        # Check for FASTA
+        gff_filename = utils.pathify(options.input_gff)
+        fasta_fname = utils.pathify(options.fasta_fname)
         flanking_introns_coords = options.flanking_introns_coords
         # if flanking_introns != None:
         #     flanking_introns = map(int, flanking_introns)            
@@ -270,9 +330,8 @@ def main():
         #                                (flanking_introns[2], flanking_introns[3])),
         #                         "dn": ((flanking_introns[0], flanking_introns[1]),
         #                                (flanking_introns[2], flanking_introns[3]))}
-        fetch_seq_from_gff(gff_filename, genome_dir, output_dir,
+        fetch_seq_from_gff(gff_filename, fasta_fname, output_dir,
                            with_flanking_introns=options.with_flanking_introns)
-
         
 if __name__ == '__main__':
     main()
